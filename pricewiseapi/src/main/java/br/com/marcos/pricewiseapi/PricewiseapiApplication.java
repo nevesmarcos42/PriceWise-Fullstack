ecfcda72ca9563af6e6720c6a2cff5bb0bf25fbb
nevesmarcos42 <nevesmarcos42@gmail.com>
2025-06-22 11:01:57 -0300

    chore: inicializa projeto Spring Boot com configuração base

---
+++ pricewiseapi/src/main/java/br/com/marcos/pricewiseapi/PricewiseapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcos.pricewiseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricewiseapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricewiseapiApplication.class, args);
+	}
+
+}
---
+++ pricewiseapi/src/test/java/br/com/marcos/pricewiseapi/PricewiseapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcos.pricewiseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricewiseapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
