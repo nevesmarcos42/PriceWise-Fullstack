946eeb5f9db59efef3f4af22c971b4929a8fb106
nevesmarcos42 <nevesmarcos42@gmail.com>
2025-06-22 13:24:26 -0300

    feat: implementa endpoint GET /products com suporte a paginação

--- pricewiseapi/src/main/java/br/com/marcos/pricewiseapi/controller/ProductController.java
+++ pricewiseapi/src/main/java/br/com/marcos/pricewiseapi/controller/ProductController.java
@@ -1,11 +1,14 @@
 package br.com.marcos.pricewiseapi.controller;
 
 import br.com.marcos.pricewiseapi.dto.CreateProductDTO;
+import br.com.marcos.pricewiseapi.dto.ProductResponseDTO;
 import br.com.marcos.pricewiseapi.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.net.URI;
 
@@ -21,6 +24,12 @@ public class ProductController {
         Long id = productService.create(dto);
         URI location = URI.create("/api/v1/products/" + id);
         return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductResponseDTO>> findAll(Pageable pageable) {
+        Page<ProductResponseDTO> result = productService.findAll(pageable);
+        return ResponseEntity.ok(result);
     }
 }
 //
