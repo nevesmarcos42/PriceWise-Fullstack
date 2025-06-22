bc1fb1692986fb2b155ad3b82b75af52b67498ab
nevesmarcos42 <nevesmarcos42@gmail.com>
2025-06-22 11:37:22 -0300

    implementa service para criação de produto com normalização e verificação de duplicidade

---
+++ pricewiseapi/src/main/java/br/com/marcos/pricewiseapi/service/ProductService.java
@@ -0,0 +1,48 @@
+package br.com.marcos.pricewiseapi.service;
+
+import br.com.marcos.pricewiseapi.dto.CreateProductDTO;
+import br.com.marcos.pricewiseapi.model.Product;
+import br.com.marcos.pricewiseapi.repository.ProductRepository;
+import jakarta.persistence.EntityExistsException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.text.Normalizer;
+import java.time.OffsetDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository repository;
+
+    public Long create(CreateProductDTO dto) {
+        String normalizedName = normalize(dto.getName());
+
+        boolean exists = repository.existsByNameIgnoreCase(normalizedName);
+        if (exists) {
+            throw new EntityExistsException("Produto com esse nome já existe.");
+        }
+
+        Product entity = Product.builder()
+                .name(dto.getName().trim())
+                .description(dto.getDescription())
+                .stock(dto.getStock())
+                .price(dto.getPrice())
+                .createdAt(OffsetDateTime.now())
+                .build();
+
+        return repository.save(entity).getId();
+    }
+
+    private String normalize(String input) {
+        if (input == null)
+            return null;
+        return Normalizer
+                .normalize(input, Normalizer.Form.NFD)
+                .replaceAll("[^\\p{ASCII}]", "")
+                .replaceAll("\\s+", " ")
+                .trim()
+                .toLowerCase();
+    }
+}
